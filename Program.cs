1a71d05607398bd09f286cc2acf552ddfaf6ece1
Simp <61764480+YungSamzy@users.noreply.github.com>
2020-12-13 16:04:53 -0600

    v1.0 - Initial Release
    This is the initial release. This may be the only release. I don't know if I'll update it or not.

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+namespace ST
+{
+    class Program
+    {
+        //Private Methods
+        static void Main()
+        {
+            Security.Initialize();
+            Menu();
+        }
+        private static void Menu()
+        {
+            Watermark();
+            AuthInitialize();
+            Console.WriteLine("1.) Login");
+            Console.WriteLine("2.) Register");
+            Console.WriteLine("Option: ");
+            string option = Console.ReadLine();
+            if (option == "1")
+            {
+                Login();
+            }
+            else if (option == "2")
+            {
+                Register();
+            }
+            else
+            {
+                MessageBox.Show("That isn't a valid option!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.Clear();
+                Main();
+            }
+        }
+        private static void Register()
+        {
+            Watermark();
+            Console.WriteLine("Username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Discord: ");
+            string email = Console.ReadLine() + "@discord.com";
+            Console.WriteLine("Token: ");
+            string token = Console.ReadLine();
+            if (AuthRegister(username, email, password, token))
+            {
+                MessageBox.Show($"Welcome, {username}!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Login();
+            }
+            else
+            {
+                MessageBox.Show("Error! Make sure you have all of your info right!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Main();
+            }
+        }
+        private static void Login()
+        {
+            Watermark();
+            Console.WriteLine("Username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+            if (AuthLogin(username, password))
+            {
+                MessageBox.Show($"Welcome, {username}!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CustomerMenu();
+            }
+            else
+            {
+                MessageBox.Show("Error! Make sure you have all of your info right!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Main();
+            }
+        }
+        private static void CustomerMenu() //Main Menu
+        {
+            Watermark();
+            MessageBox.Show("Spoofing!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Console.WriteLine("Loading Files...");
+            Thread.Sleep(2000);
+            Directory.CreateDirectory(@"C:\ProgramData\SpooferTemplate\");
+            string mapper = @"C:\ProgramData\SpooferTemplate\Local64.exe";
+            string driver1 = @"C:\ProgramData\SpooferTemplate\Local64.sys";
+            string driver2 = @"C:\ProgramData\SpooferTemplate\Local64cfg.sys";
+            var drivers = driver1 + driver2;
+            WebClient files = new WebClient();
+            Security.DownloadFile("https://cdn.discordapp.com/attachments/711016214998024225/787187597427474432/Local64.exe", mapper); //change all of these
+            Security.DownloadFile("https://cdn.discordapp.com/attachments/711016214998024225/787187598908063754/Local64.sys", driver1);
+            Security.DownloadFile("https://cdn.discordapp.com/attachments/711016214998024225/787187600540434442/Local64cfg.sys", driver2);
+            Console.WriteLine("Done!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Checking File Integrity...");
+            Thread.Sleep(2000);
+            Security.CheckHash(mapper, "mglxA3hd3qHp25fCrHQ0eZeRPnBbBWS+hwxEcEw2tgQ="); //change all of these
+            Security.CheckHash(driver1, "/L5BeDLXcE169CwUIuIuyL9feodJ98lL5gKvncC9Wts=");
+            Security.CheckHash(driver2, "MfTPtMcdpEEgdSchEDoWUSREwTwqwthXp+bxPLZ5tCc=");
+            Console.WriteLine("Done!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Mapping Drivers...");
+            Thread.Sleep(2000);
+            //Process.Start(mapper, drivers);
+            Console.WriteLine("Done!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Cleaning Up...");
+            Thread.Sleep(2000);
+            Security.CleanUp();
+            Console.WriteLine("Done!");
+            Thread.Sleep(2000);
+            MessageBox.Show("Spoofed!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Main();
+        }
+
+        //Public Methods
+        public static void Watermark()
+        {
+            Console.Clear();
+            Console.Title = "Spoofer Template | Simp#0174";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("                                                     Spoofer Template");
+            Console.WriteLine("                                                         Simp#0174");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void Wait() => Console.ReadKey();
+        public static void AuthInitialize() => auth_instance.init();
+        public static bool AuthLogin(string username, string password) => auth_instance.login(username, password);
+        public static bool AuthRegister(string username, string email, string password, string token) => auth_instance.register(username, email, password, token);
+
+        public static api auth_instance = new api("version", "program_key", "api_key", show_messages: false); //change this
+    }
+}
---
+++ Security.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ST
+{
+    class Security
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool CheckRemoteDebuggerPresent(IntPtr hProcess, ref bool isDebuggerPresent);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        static bool isDebuggerPresent = false;
+        public static void Initialize()
+        {
+            DetectSandboxie();
+            AntiDebug();
+            AntiDnspy();
+            DetectVM();
+        }
+        private static bool PinPublicKey(object sender, X509Certificate certificate,
+                                 X509Chain chain, SslPolicyErrors sslPolicyErrors) //The SSL Pinning
+        {
+            if (certificate == null || chain == null)
+                return false;
+
+            if (sslPolicyErrors != SslPolicyErrors.None)
+                return false;
+
+            string pk = certificate.GetPublicKeyString(); //if you ever need to update just make it wirteout pk and then update it. (Console.WriteLien(pk);)
+            return pk.Equals("04054F9BE28FAAF5B5F960DB2B3E5D743C9B6B8435B762E704F8E07797C12E2F7630EB7DE33A6EE5DDA3A814E40C05FE95822CDB4F732AA062F357BC3FBE0214E8"); //you may need to update this if discord changes it
+        }
+        private static void AntiDnspy()
+        {
+            IntPtr kernel32 = LoadLibrary("kernel32.dll");
+            IntPtr GetProcessId = GetProcAddress(kernel32, "IsDebuggerPresent");
+            byte[] data = new byte[1];
+            Marshal.Copy(GetProcessId, data, 0, 1);
+            if (data[0] == 0xE9)
+            {
+                MessageBox.Show("Common Debugger Detected!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                CleanUp();
+                Destruct();
+                return;
+            }
+            GetProcessId = GetProcAddress(kernel32, "CheckRemoteDebuggerPresent");
+            data = new byte[1];
+            Marshal.Copy(GetProcessId, data, 0, 1);
+            if (data[0] == 0xE9)
+            {
+                MessageBox.Show("Common Debugger Detected!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                CleanUp();
+                Destruct();
+                return;
+            }
+        }
+        private static void AntiDebug()
+        {
+            CheckRemoteDebuggerPresent(Process.GetCurrentProcess().Handle, ref isDebuggerPresent);
+            if (isDebuggerPresent)
+            {
+                MessageBox.Show("Common Debugger Detected!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                CleanUp();
+                Destruct();
+            }
+        }
+        private static void DetectSandboxie()
+        {
+            if (GetModuleHandle("SbieDll.dll").ToInt32() != 0)
+            {
+                MessageBox.Show("Sandboxie Detected!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                CleanUp();
+                Destruct();
+            }
+            else
+            {
+            }
+        }
+        private static void DetectVM()
+        {
+            using (ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher("Select * from Win32_ComputerSystem"))
+            {
+                using (ManagementObjectCollection managementObjectCollection = managementObjectSearcher.Get())
+                {
+                    foreach (ManagementBaseObject managementBaseObject in managementObjectCollection)
+                    {
+                        if ((managementBaseObject["Manufacturer"].ToString().ToLower() == "microsoft corporation" && managementBaseObject["Model"].ToString().ToUpperInvariant().Contains("VIRTUAL")) || managementBaseObject["Manufacturer"].ToString().ToLower().Contains("vmware") || managementBaseObject["Model"].ToString() == "VirtualBox")
+                        {
+                            MessageBox.Show("VM Detected!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            CleanUp();
+                            Destruct();
+                        }
+                    }
+                }
+            }
+            foreach (ManagementBaseObject managementBaseObject2 in new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController").Get())
+            {
+                if (managementBaseObject2.GetPropertyValue("Name").ToString().Contains("VMware") && managementBaseObject2.GetPropertyValue("Name").ToString().Contains("VBox"))
+                {
+                    MessageBox.Show("VM Detected!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    CleanUp();
+                    Destruct();
+                }
+            }
+        }
+        public static void DownloadFile(string url, string path)
+        {
+            ProxyCheck(); //outdated, meant for before SSL pinning
+            Thread.Sleep(1000);
+            HttpWebRequest.DefaultWebProxy = new WebProxy(); //outdated, meant for before SSL pinning
+            Thread.Sleep(1000);
+            WebClient files = new WebClient();
+            ServicePointManager.ServerCertificateValidationCallback = PinPublicKey;
+            Thread.Sleep(1000);
+            try
+            {
+                files.DownloadFile(url, path);
+            }
+            catch (Exception ex)
+            {
+                if ($"{ex}".Contains("The underlying connection was closed: Could not establish trust relationship for the SSL/TLS secure channel."))
+                {
+                    MessageBox.Show("The underlying connection was closed: Could not establish trust relationship for the SSL/TLS secure channel.", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    CleanUp();
+                    Destruct();
+                }
+                else
+                {
+                    MessageBox.Show($"{ex}", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    CleanUp();
+                    Destruct();
+                }
+
+            }
+        }
+        public static void CleanUp()
+        {
+            try
+            {
+                string[] files2 = Directory.GetFiles(@"C:\ProgramData\SpooferTemplate\");
+                foreach (string file in files2)
+                {
+                    File.Delete(file);
+                }
+                Directory.Delete(@"C:\ProgramData\SpooferTemplate\");
+            }
+            catch
+            {
+
+            }
+        }
+        public static void CheckHash(string file, string original)
+        {
+            string sourceFileName = file;
+            byte[] shaHash;
+            using (var shaForStream = new SHA256Managed())
+            using (Stream sourceFileStream = File.Open(sourceFileName, FileMode.Open))
+            using (Stream sourceStream = new CryptoStream(sourceFileStream, shaForStream, CryptoStreamMode.Read))
+            {
+                while (sourceStream.ReadByte() != -1) ;
+                shaHash = shaForStream.Hash;
+            }
+            string hash = Convert.ToBase64String(shaHash);
+            if (hash == original)
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("File Integreity Check Failed! Possible File Tampering!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                CleanUp();
+                Destruct();
+            }
+        }
+        private static void Destruct() //for some reason, this doesn't seem to work for me, maybe it will work for you?
+        {
+            string app = System.AppDomain.CurrentDomain.FriendlyName;
+            string AppPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).ToString() + $@"\{app}";
+            Process.Start("cmd.exe", "/C ping 1.1.1.1 -n 1 -w 3000 > Nul & Del " + AppPath);
+            Process.GetCurrentProcess().Kill();
+        }
+        private static void ProxyCheck() //I had this before SSL pinning
+        {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            string ProxyEnabledOrNo = registry.GetValue("ProxyEnable").ToString();
+            object ProxyServerValue = registry.GetValue("ProxyServer");
+            if (ProxyEnabledOrNo == "1")
+            {
+                MessageBox.Show("Error! Network Debugger Detected! Closing Now!", "Spoofer Template", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Destruct();
+            }
+        }
+        
+    }
+}
